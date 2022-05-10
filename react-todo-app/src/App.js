d0dc636d64942ca71a52dafcb95b01e017ce6a6c
praneshulleri <ipraneshulleri@gmail.com>
2022-05-10 09:45:10 +0300

    changes

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code>
-          hey hello
+          hey hello targets
         </p>
         <a
           className="App-link"
