80cc94f35803f05c50139494dbb2b2b3ee0373c5
praneshulleri <ipraneshulleri@gmail.com>
2022-05-15 12:06:23 +0300

    navbar and sharedlayout

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -3,17 +3,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutPage from './pages/AboutPage';
 import ProductPage from './pages/ProductPage';
+import SharedLayout from './pages/SharedLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />}>
+        <Route exact path="/" element={<SharedLayout />}>
+          <Route index element={<Home />}></Route>
           <Route exact path="/product" element={<ProductPage />}></Route>
           <Route path="/product/:id"></Route>
           <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<div>Error</div>} />
         </Route>
-        <Route path="*" element={<div>Error</div>} />
       </Routes>
     </BrowserRouter>
   );
--- react-router/src/pages/Home.js
+++ react-router/src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      Home Component
-      <Outlet />
-    </div>
+    <>
+      <section className="section">home page components </section>
+    </>
   );
 };
 
