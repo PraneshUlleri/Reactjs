7ecfb26f2c721cae613ebea9178b23625aaf4821
praneshulleri <ipraneshulleri@gmail.com>
2022-05-19 19:10:43 +0300

    state hooks2

---
+++ react-hooks/src/UseState/StateHooks2.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState } from 'react';
+const StateHooks2 = () => {
+  const [InputValue, setInputValue] = useState('Value');
+  const iv = (event) => {
+    const newvalue = event.target.value;
+    setInputValue(newvalue);
+  };
+  return (
+    <div>
+      <input placeholder="enter some value" onChange={iv} />
+      {InputValue}
+    </div>
+  );
+};
+
+export default StateHooks2;
