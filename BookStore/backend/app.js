6f02edf1cc65d26bbdcd7f027dbeff1384b8c106
praneshulleri <ipraneshulleri@gmail.com>
2022-05-20 14:31:00 +0300

    mvc created

--- BookStore/backend/app.js
+++ BookStore/backend/app.js
@@ -1,1 +1,19 @@
 console.log('hello world');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:root121212@cluster0.egvcx.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('connection established!'))
+  .then(() => app.listen(5000))
+  .catch((err) => console.log(err));
+
+// middleware
+
+app.use('/', (req, res, next) => {
+  res.send('this');
+});
