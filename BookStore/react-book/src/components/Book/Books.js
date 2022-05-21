3d0028489b40151473b5fe5465cdcc4522fe8b81
praneshulleri <ipraneshulleri@gmail.com>
2022-05-21 14:08:34 +0300

    added axios

--- BookStore/react-book/src/components/Book/Books.js
+++ BookStore/react-book/src/components/Book/Books.js
@@ -1,5 +1,9 @@
 import React from 'react';
+const URL = "http:'localhost:5000/books";
 
+const fetchHandler = async () => {
+  return await axios.get(URL).then((res) => res.data);
+};
 const Books = () => {
   return <div>Books</div>;
 };
