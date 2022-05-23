37c5377f55d8ab284770460d358d5460405596a4
praneshulleri <ipraneshulleri@gmail.com>
2022-05-23 15:50:25 +0300

    connection to the mongodb established

--- Auth/backend/app.js
+++ Auth/backend/app.js
@@ -1,1 +1,14 @@
-console.log('he!');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:root121212@cluster0.b7ero.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    app.listen(5000);
+    console.log('Database Connection is Established');
+  })
+  .catch((err) => console.log(err));
