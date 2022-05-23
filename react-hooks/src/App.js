bcc1cb04f224aeaffc7887ae7424de9d33b5de3e
praneshulleri <ipraneshulleri@gmail.com>
2022-05-23 12:10:31 +0300

    the effect hook is implemented

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -3,12 +3,15 @@
 // import StateHooks from './UseState/StateHooks';
 // import StateHooks2 from './UseState/StateHooks2';
 
+import EffectHook from './useeffect/EffectHook';
+
 function App() {
   return (
     <div>
       {/* <StateHooks />
       <StateHooks2 />
       <Reducer /> */}
+      <EffectHook />
     </div>
   );
 }
--- react-hooks/src/useeffect/EffectHook.js
+++ react-hooks/src/useeffect/EffectHook.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 const EffectHook = () => {
-  return <div>EffectHook</div>;
+  const [data, setData] = useState('');
+  useEffect(() => {
+    axios.get('http://jsonplaceholder.typicode.com/comments').then((res) => {
+      setData(res.data[0].email);
+      console.log('12');
+    });
+  }, []);
+
+  return <div>EffectHook Example {data}</div>;
 };
 
 export default EffectHook;
