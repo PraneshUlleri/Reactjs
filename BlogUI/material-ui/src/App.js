026ff9039444c8dd3229402230265ded98db8ae2
praneshulleri <ipraneshulleri@gmail.com>
2022-05-24 10:24:51 +0300

    made use of buttons

--- BlogUI/material-ui/src/App.js
+++ BlogUI/material-ui/src/App.js
@@ -1,5 +1,50 @@
+import { PhotoCamera } from '@mui/icons-material';
+import { Person } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import { Input } from '@mui/material';
+import { Button } from '@mui/material';
+
 function App() {
-  return <div>hellow!</div>;
+  return (
+    <div>
+      hellow!
+      <Button
+        variant="contained"
+        color="secondary"
+        size="large"
+        startIcon={<Person />}
+        onClick={() => {
+          alert('clicked');
+        }}
+      >
+        Contained
+      </Button>
+      <div>
+        <label htmlFor="contained-button-file">
+          <Input
+            accept="image/*"
+            id="contained-button-file"
+            multiple
+            type="file"
+          />
+          <Button variant="contained" component="span">
+            Upload
+          </Button>
+        </label>
+        <p></p>
+        <label htmlFor="icon-button-file">
+          <Input accept="image/*" id="icon-button-file" type="file" />
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            <PhotoCamera />
+          </IconButton>
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default App;
