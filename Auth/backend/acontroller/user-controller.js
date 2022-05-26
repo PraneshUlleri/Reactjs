5c9549acd911f986972869f00d44884da39d6d50
praneshulleri <ipraneshulleri@gmail.com>
2022-05-26 11:13:34 +0300

    login function created

--- Auth/backend/acontroller/user-controller.js
+++ Auth/backend/acontroller/user-controller.js
@@ -1,9 +1,10 @@
 const User = require('../amodel/user-model');
+const bcrypt = require('bcryptjs');
 
 const signUp = async (req, res, next) => {
   const { name, email, password } = req.body;
-  const user = new User({ name, email, password });
 
+  // checking for exisiting user
   let existingUser;
   try {
     existingUser = await User.findOne({
@@ -17,14 +18,41 @@ const signUp = async (req, res, next) => {
     return res.status(400).json({ message: 'user already exist' });
   }
 
-  //   break....
+  //bcrypt
+  const hashedPassword = bcrypt.hashSync(password);
+
+  // create the user
+  const user = new User({ name, email, password: hashedPassword });
   try {
     await user.save();
   } catch (error) {
     console.log('error: ' + error + ' is the error');
   }
 
+  //return when everything is alright
   return res.status(201).json({ message: user });
 };
 
+const logIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  let exisitingUser;
+  try {
+    exisitingUser = await User.findOne({ email: email });
+  } catch (error) {
+    console.log('error is : ' + error);
+  }
+  if (!exisitingUser)
+    return res.status(400).json({ message: 'user not found. sign up please' });
+
+  const ispasswordCorrect = bcrypt.compareSync(
+    password,
+    exisitingUser.password
+  );
+  if (!ispasswordCorrect) {
+    return res.status(400).json({ message: 'invalid email/ password' });
+  }
+  return res.status(200).json({ message: 'sucessfully logged in ' });
+};
+
 exports.signUp = signUp;
+exports.logIn = logIn;
--- Auth/backend/aroute/user-routes.js
+++ Auth/backend/aroute/user-routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signUp } = require('../acontroller/user-controller');
+const { signUp, logIn } = require('../acontroller/user-controller');
 router.get('/', (req, res, next) => {
   res.send('heeee');
 });
 
 router.post('/signup', signUp);
-
+router.post('/login', logIn);
 module.exports = router;
