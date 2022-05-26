1bcd136d609e315f9cc221a8c218cad14173eee6
praneshulleri <ipraneshulleri@gmail.com>
2022-05-26 10:32:32 +0300

    logic to check if the user has already signed up

--- Auth/backend/acontroller/user-controller.js
+++ Auth/backend/acontroller/user-controller.js
@@ -4,6 +4,20 @@ const signUp = async (req, res, next) => {
   const { name, email, password } = req.body;
   const user = new User({ name, email, password });
 
+  let existingUser;
+  try {
+    existingUser = await User.findOne({
+      email: email,
+    });
+  } catch (error) {
+    console.log('error is : ' + error);
+  }
+
+  if (existingUser) {
+    return res.status(400).json({ message: 'user already exist' });
+  }
+
+  //   break....
   try {
     await user.save();
   } catch (error) {
