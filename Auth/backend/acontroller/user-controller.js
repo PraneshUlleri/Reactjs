2a8cf17bafed05944a4761778374eb77fdc39d8d
praneshulleri <ipraneshulleri@gmail.com>
2022-05-26 12:06:34 +0300

    token authorization

--- Auth/backend/acontroller/user-controller.js
+++ Auth/backend/acontroller/user-controller.js
@@ -1,6 +1,9 @@
 const User = require('../amodel/user-model');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET_KEY = 'mysecretkey';
 
+//signup function
 const signUp = async (req, res, next) => {
   const { name, email, password } = req.body;
 
@@ -33,6 +36,7 @@ const signUp = async (req, res, next) => {
   return res.status(201).json({ message: user });
 };
 
+//login function
 const logIn = async (req, res, next) => {
   const { email, password } = req.body;
   let exisitingUser;
@@ -51,8 +55,48 @@ const logIn = async (req, res, next) => {
   if (!ispasswordCorrect) {
     return res.status(400).json({ message: 'invalid email/ password' });
   }
-  return res.status(200).json({ message: 'sucessfully logged in ' });
+  const token = jwt.sign({ id: exisitingUser._id }, JWT_SECRET_KEY, {
+    expiresIn: '1hr',
+  });
+  return res.status(200).json({
+    message: 'sucessfully logged in ' + exisitingUser + ' TOKEN: ' + token,
+  });
+};
+
+// authorization
+const verifyToken = async (req, res, next) => {
+  const header = req.headers[`authorization`];
+  //   console.log(header);
+  const token = header.split(' ')[1];
+  if (!token) {
+    res.status(404).json({ message: 'No token' });
+  }
+  jwt.verify(String(token), JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      res.status(400).json({ message: 'Invalid token' });
+    }
+    console.log(user.id);
+    req.id = user.id;
+  });
+  next();
+};
+
+// get the details of the user
+const getUser = async (req, res, next) => {
+  const userId = req.id;
+  let user;
+  try {
+    user = await User.findById(userId, '-password');
+  } catch (error) {
+    return new Error(error);
+  }
+  if (!user) {
+    return res.status(404).json({ message: 'user not found' });
+  }
+  return res.status(200).json({ user });
 };
 
 exports.signUp = signUp;
 exports.logIn = logIn;
+exports.verifyToken = verifyToken;
+exports.getUser = getUser;
--- Auth/backend/aroute/user-routes.js
+++ Auth/backend/aroute/user-routes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { signUp, logIn } = require('../acontroller/user-controller');
+const {
+  signUp,
+  logIn,
+  verifyToken,
+  getUser,
+} = require('../acontroller/user-controller');
 router.get('/', (req, res, next) => {
   res.send('heeee');
 });
 
 router.post('/signup', signUp);
 router.post('/login', logIn);
+router.get('/usertok', verifyToken, getUser);
+
 module.exports = router;
