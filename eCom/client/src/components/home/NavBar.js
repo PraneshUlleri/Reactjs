580f76b318a9953706b174a7df19ccd414ded54a
praneshulleri <ipraneshulleri@gmail.com>
2022-06-01 15:39:00 +0300

    added multi carousel

--- eCom/client/src/components/home/NavBar.js
+++ eCom/client/src/components/home/NavBar.js
@@ -1,16 +1,25 @@
+import { Typography } from '@mui/material';
 import { styled } from '@mui/material';
 import { Box } from '@mui/material';
+import { textAlign } from '@mui/system';
 import { style } from '@mui/system';
 import React from 'react';
 import { navData } from '../../constants/data';
 
 const Compond = styled(Box)`
   display: flex;
-  margin: 55pz 130px 0 130px;
+  margin: 55px 50px 0 50px;
   justify-content: space-between;
 `;
 const Container = styled(Box)`
-padding 12px 8px`;
+padding 12px 8px;
+text-align: center`;
+
+const Text = styled(Typography)`
+  font-size: 14px;
+  font-weight: 600;
+  font-family: inherit;
+`;
 
 const NavBar = () => {
   return (
@@ -18,7 +27,7 @@ const NavBar = () => {
       {navData.map((data) => (
         <Container>
           <img src={data.url} alt="nav" style={{ width: 64 }} />
-          <p>{data.text}</p>
+          <Text>{data.text}</Text>
         </Container>
       ))}
     </Compond>
