73c372bed7e4d6e472cae1682dd868372e585946
praneshulleri <ipraneshulleri@gmail.com>
2022-06-02 08:22:05 +0300

    created a banner

--- eCom/client/src/components/home/Home.js
+++ eCom/client/src/components/home/Home.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import { Box } from '@mui/material';
+import React, { Fragment } from 'react';
+import Banner from './Banner';
 import NavBar from './NavBar';
 
 const Home = () => {
-  return <NavBar />;
+  return (
+    <Fragment>
+      <NavBar />
+      <Banner />
+    </Fragment>
+  );
 };
 
 export default Home;
