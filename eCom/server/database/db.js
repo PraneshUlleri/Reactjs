58530ef93822fd9922e5f2a5e75287154ccff60f
praneshulleri <ipraneshulleri@gmail.com>
2022-06-02 10:26:02 +0300

    installed dot env

--- eCom/server/database/db.js
+++ eCom/server/database/db.js
@@ -1,5 +1,12 @@
-export const Connection = () => {
+import mongoose from 'mongoose';
+export const Connection = async () => {
   try {
+    await mongoose
+      .connect(
+        `mongodb+srv://admin:root121212@cluster0.dtjlr.mongodb.net/?retryWrites=true&w=majority`,
+        { useUnifiedTopology: true, useNewUrlParser: true }
+      )
+      .then(console.log('db connected'));
   } catch (error) {
     console.log('Error while connecting with the data base :' + error);
   }
