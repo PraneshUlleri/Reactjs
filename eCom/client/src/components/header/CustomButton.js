56ca1a61a0a934c27f7a19f12ca44d8a0ba442b2
praneshulleri <ipraneshulleri@gmail.com>
2022-06-02 15:48:31 +0300

    added few css

--- eCom/client/src/components/header/CustomButton.js
+++ eCom/client/src/components/header/CustomButton.js
@@ -4,18 +4,27 @@ import { Box } from '@mui/material';
 import React from 'react';
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 import styled from '@emotion/styled';
+import { useState } from 'react';
+import LoginDialog from '../login/LoginDialog';
 
 const CustomButton = () => {
+  const [open, setOpen] = useState(false);
+  const loginDialog = () => {
+    setOpen(true);
+  };
   return (
     <div>
       <Box style={{ display: 'flex', margin: '10' }}>
-        <Button variant="contained">login</Button>
+        <Button variant="contained" onClick={() => loginDialog()}>
+          login
+        </Button>
         <Typography>Seller&nbsp;</Typography>
         <Typography>More </Typography>
         <Box style={{ display: 'flex' }}>
           <ShoppingCartCheckoutIcon />
           <Typography>Cart</Typography>
         </Box>
+        <LoginDialog open={open} setOpen={setOpen} />
       </Box>
     </div>
   );
--- eCom/client/src/components/login/LoginDialog.js
+++ eCom/client/src/components/login/LoginDialog.js
@@ -1,6 +1,56 @@
 import { Box } from '@mui/material';
+import { TextField } from '@mui/material';
+import { styled } from '@mui/material';
+import { Button } from '@mui/material';
+import { Typography } from '@mui/material';
 import { Dialog } from '@mui/material';
+import { maxHeight } from '@mui/system';
+
 import React from 'react';
+
+const Component = styled(Box)`
+  height: 80vh;
+  width: 90vh;
+  display: flex;
+`;
+const Image = styled(Box)`
+  background: #0055ff
+    url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png)
+    center 35% no-repeat;
+
+  height: 100%;
+  width: 40%;
+  padding: 45px 35px;
+  color: white;
+`;
+
+const Wrapper = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  padding: 25px 35px;
+  flex: 2;
+  & > div,
+  & > button,
+  & > p {
+    margin-top: 20px;
+  }
+`;
+
+const LoginButton = styled(Button)`
+text-transform:none; background: blue; color: white;
+height: 40px, border-radius:2px, box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20%)`;
+
+const OtpButton = styled(Button)`
+text-transform:none; 
+background: white; 
+height: 40px,
+ border-radius:2px, 
+box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20%)`;
+
+const Text = styled(Typography)`
+  font-size: 12px;
+  text-align: center;
+`;
 
 const LoginDialog = ({ open, setOpen }) => {
   const handleClose = () => {
@@ -8,11 +58,31 @@ const LoginDialog = ({ open, setOpen }) => {
   };
 
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <Box>
-        <Box></Box>
-        <Box></Box>
-      </Box>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      PaperProps={{ sx: { maxWidth: 'unset', maxHeight: 'unset' } }}
+    >
+      <Component>
+        <Image>
+          <Typography variant="h5">Login</Typography>
+          <Typography style={{ marginTop: 20 }}>blablabla</Typography>
+        </Image>
+
+        <Wrapper>
+          <TextField
+            id="emailnumber"
+            label="enter email/mobile"
+            variant="outlined"
+          />
+          <TextField id="password" label="enter password" variant="outlined" />
+          <Typography>blablabla</Typography>
+          <LoginButton>login</LoginButton>
+          <Typography>Ok</Typography>
+          <OtpButton>request otp</OtpButton>
+          <Text>New Sign up!</Text>
+        </Wrapper>
+      </Component>
     </Dialog>
   );
 };
