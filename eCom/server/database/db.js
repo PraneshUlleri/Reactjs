0192ae5bec979d84c0d90da4e12eb5cd616aa29d
praneshulleri <ipraneshulleri@gmail.com>
2022-06-02 10:13:02 +0300

    added connection and installed mongoose

---
+++ eCom/server/database/db.js
@@ -0,0 +1,7 @@
+export const Connection = () => {
+  try {
+  } catch (error) {
+    console.log('Error while connecting with the data base :' + error);
+  }
+};
+export default Connection;
--- eCom/server/index.js
+++ eCom/server/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import Connection from './database/db.js';
 
 const app = express();
-
 const port = 5000;
+
+Connection();
 
 app.listen(port, () => {
   console.log('Running at' + port);
