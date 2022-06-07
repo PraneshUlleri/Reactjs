a33b1e0abaa5654a6996e0dfdafd33e65894a9d1
praneshulleri <ipraneshulleri@gmail.com>
2022-06-07 13:04:54 +0300

    added model

---
+++ AMernBlog/backend/amodel/blog-model.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema();
+
+const blogSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  user: { type: String, require: true },
+});
