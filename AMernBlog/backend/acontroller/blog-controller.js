cec9408c7e819d69568509b41080503a52c35cc1
praneshulleri <ipraneshulleri@gmail.com>
2022-06-07 15:29:23 +0300

    added blogs and connected it to user

--- AMernBlog/backend/acontroller/blog-controller.js
+++ AMernBlog/backend/acontroller/blog-controller.js
@@ -1,4 +1,6 @@
+import mongoose from 'mongoose';
 import Blogs from '../amodel/blog-model';
+import Users from '../amodel/user-model';
 
 export const getAllBlogs = async (req, res, next) => {
   let allBlogs;
@@ -13,4 +15,86 @@ export const getAllBlogs = async (req, res, next) => {
   return res.status(200).json({ allBlogs });
 };
 
-export const addBlog = async (req, res, next) => {};
+export const addBlog = async (req, res, next) => {
+  const { title, description, image, user } = req.body;
+  let existingUser;
+  try {
+    existingUser = await Users.findById(user);
+  } catch (error) {
+    return console.log(error);
+  }
+
+  if (!existingUser) {
+    return res.status(400).json({ message: 'user not existing' });
+  }
+
+  const newBlog = new Blogs({
+    title,
+    description,
+    image,
+    user,
+  });
+
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await newBlog.save({ session });
+    existingUser.blogs.push(newBlog);
+    await existingUser.save({ session });
+    await session.commitTransaction();
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+  return res.status(200).json({ newBlog });
+};
+
+export const updateBlog = async (req, res, next) => {
+  const { title, description } = req.body;
+  const blogId = req.params.id;
+  let updatedBlog;
+  try {
+    updatedBlog = await Blogs.findByIdAndUpdate(blogId, {
+      title,
+      description,
+    });
+  } catch (error) {
+    return console.log(error);
+  }
+
+  if (!updatedBlog) {
+    return res.status(500).json({ message: 'blogs cannot be updated' });
+  }
+
+  return res.status(200).json({ updatedBlog });
+};
+
+export const getOneBlog = async (req, res, next) => {
+  const { title, description, image, user } = req.body;
+  const blogId = req.params.id;
+  let blogById;
+  try {
+    blogById = await Blogs.findById(blogId);
+  } catch (error) {
+    return console.log(error);
+  }
+  if (!blogById) {
+    return res.status(404).json({ message: ' no blog' });
+  }
+  return res.status(200).json({ blogById });
+};
+
+export const deleteBlog = async (req, res, next) => {
+  const { title, description, image, user } = req.body;
+  const blogId = req.params.id;
+  let blogById;
+  try {
+    blogById = await Blogs.findByIdAndRemove(blogId);
+  } catch (error) {
+    return console.log(error);
+  }
+
+  if (!blogById) {
+    return res.status(500).json({ message: 'unable to delete' });
+  }
+  return res.status(200).json({ message: 'successfully deleted' });
+};
