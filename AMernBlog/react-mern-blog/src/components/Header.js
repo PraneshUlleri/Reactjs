2bc21b7b8f097ffc0a8d8c099848b8c84ee648ac
praneshulleri <ipraneshulleri@gmail.com>
2022-06-09 16:06:03 +0300

    added a few stuff

--- AMernBlog/react-mern-blog/src/components/Header.js
+++ AMernBlog/react-mern-blog/src/components/Header.js
@@ -3,15 +3,26 @@ import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
 
 const Header = () => {
   return (
-    <AppBar>
-      <Toolbar>
-        <Typography variant="h4">BlogsApp</Typography>
-        <Box>
-          <Button color="warning ">Login</Button>
-          <Button color="warning">Signup</Button>
-        </Box>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <AppBar
+        position="static "
+        sx={{
+          background:
+            'linear-gradient(174deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)',
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h4">hello</Typography>
+          <Box display="flex" marginLeft="auto">
+            <Button variant="contained " sx={{ margin: 1, borderRadius: 10 }}>
+              {' '}
+              signup
+            </Button>
+            <Button sx={{ margin: 1, borderRadius: 10 }}> login</Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
