2afbf3006f03870dc184abe78c72a9dbde7cc8f1
praneshulleri <ipraneshulleri@gmail.com>
2022-06-09 15:11:43 +0300

    header

--- AMernBlog/react-mern-blog/src/components/Header.js
+++ AMernBlog/react-mern-blog/src/components/Header.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import MenuIcon from '@mui/icons-material';
-import {
-  AppBar,
-  Box,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
-} from '@mui/material';
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+
 const Header = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h4">BlogsApp</Typography>
+        <Box>
+          <Button color="warning ">Login</Button>
+          <Button color="warning">Signup</Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
