cc3e462fb480b975a0b2113584f0ae4ce86cdff0
praneshulleri <ipraneshulleri@gmail.com>
2022-06-15 15:17:58 +0300

    creating auth

--- AMernBlog/react-mern-blog/src/components/Auth.js
+++ AMernBlog/react-mern-blog/src/components/Auth.js
@@ -1,7 +1,26 @@
+import { Box, Button, TextField, Typography } from '@mui/material';
 import React from 'react';
 
 const Auth = () => {
-  return <div>login</div>;
+  return (
+    <div>
+      <form>
+        <Box
+          display="flex"
+          flexDirection={'column'}
+          alignItems="center"
+          boxShadow="10px"
+        >
+          <Typography> Login</Typography>
+          <TextField />
+          <TextField />
+          <TextField />
+          <Button> Submit</Button>
+          <Button>Change to signup</Button>
+        </Box>
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
