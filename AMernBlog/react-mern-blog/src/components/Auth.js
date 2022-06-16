76b4313cf1842faf1f23eb7c216564249ea060b2
praneshulleri <ipraneshulleri@gmail.com>
2022-06-16 15:26:37 +0300

    added the handle change

--- AMernBlog/react-mern-blog/src/components/Auth.js
+++ AMernBlog/react-mern-blog/src/components/Auth.js
@@ -2,7 +2,21 @@ import { Box, Button, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 
 const Auth = () => {
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
   const [isSignup, setIsSignup] = useState(false);
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <div>
       <form>
@@ -20,9 +34,28 @@ const Auth = () => {
           <Typography variant="h4" padding={3} textAlign="center">
             {isSignup ? 'Login' : 'Signup'}
           </Typography>
-          {isSignup && <TextField margin="normal" placeholder="Name" />}
-          <TextField margin="normal" placeholder="Email" type={'email'} />
-          <TextField margin="normal" placeholder="Password" type={'password'} />
+          {isSignup && (
+            <TextField
+              margin="normal"
+              placeholder="Name"
+              onChange={handleChange}
+              name="name"
+            />
+          )}
+          <TextField
+            margin="normal"
+            placeholder="Email"
+            type={'email'}
+            onChange={handleChange}
+            name="email"
+          />
+          <TextField
+            margin="normal"
+            placeholder="Password"
+            type={'password'}
+            onChange={handleChange}
+            name="password"
+          />
           <Button
             variant="contained"
             sx={{ borderRadius: 3, marginTop: 3 }}
