6d37c9a6f528552d4a4ee2ac7fe0daeabc3213e6
praneshulleri <ipraneshulleri@gmail.com>
2022-06-20 13:56:23 +0300

    added blogs

--- AMernBlog/react-mern-blog/src/components/Blogs.js
+++ AMernBlog/react-mern-blog/src/components/Blogs.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import axios from 'axios';
+import { set } from 'immer/dist/internal';
+import React, { useEffect, useState } from 'react';
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState();
+  const sendRequest = async () => {
+    const res = await axios
+      .get(`http://localhost:5000/blog/all`)
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+
+  useEffect(() => {
+    sendRequest().then((data) => console.log(data.blogs));
+  }, []);
+
   return <div>Blogs</div>;
 };
 
